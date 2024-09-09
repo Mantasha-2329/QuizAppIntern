7f621e5dcd3fe88d4935953f859c363f0fcc2f18
Mantasha-2329 <146323803+Mantasha-2329@users.noreply.github.com>
2024-09-09 18:33:27 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Who is considered the 'father of the modern computer'?",
+            "Java was developed by.....?",
+            "Who is the founder of Google ?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Alan Turing" , "Charles Babbage" , "John von Neumann" , "Bill Gates"},
+            {"Dennis Ritchie" , "Bill Gates" , "James Gosling" , "None of the above"},
+            {"Larry Page" , "Sergey Brin" , "Mark Zuckerberg" , "John von Neumann"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "Charles Babbage" ,
+            "James Gosling",
+            "Larry Page",
+            "Elon Musk",
+            "All of the above"
+    };
+}
